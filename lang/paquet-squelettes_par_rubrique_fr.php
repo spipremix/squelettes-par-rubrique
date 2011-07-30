a69490e1b015f7cbe21f30282eb70392fbb8d502
eric <eric@smellup.net>
2011-07-30 14:52:13 +0000

    Début des tests du nouveau fichier paquet.xml. On ajoute pour chaque plugin du core : - le paquet.xml sous le nom _paquet.xml pour éviter une utilisation par défaut à la place de plugin.xml Il suffira de le renommer pour effectuer les tests. - les fichiers de langue nécessaire à la description et au slogan. - un fichier _paquet-migration qui contient les commandes svn pour migrer ou ajouter des fichiers. Ce fichier n'a plus d'autre intérêt que de lister les fichiers à renommer pour retrouver le fonctionnement des balises disparues : install, fonctions et options.
    git-svn-id: svn://zone.spip.org/spip-zone/_core_/plugins/squelettes_par_rubrique@50113 ac52e18a-acf5-0310-9fe8-c4428f23b10a

---
+++ lang/paquet-squelettes_par_rubrique_fr.php
@@ -0,0 +1,19 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+// Fichier produit par PlugOnet
+// Module: paquet-squelettes_par_rubrique
+// Langue: fr
+// Date: 30-07-2011 15:01:38
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// S
+	'squelettes_par_rubrique_description' => 'Support des squelettes suffixés par numéro de rubrique et/ou par code de langue :
+(-23.html, =23.html, et .en.html)',
+	'squelettes_par_rubrique_slogan' => 'Support des squelettes suffixés dans SPIP',
+);
+?>
