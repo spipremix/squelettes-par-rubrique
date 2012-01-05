eddb41079de05468f8b423b0f36f45689de2f181
salvatore <salvatore@rezo.net>
2012-01-05 08:09:28 +0000

    langues (paquet-squelettes_par_rubrique)

--- lang/paquet-squelettes_par_rubrique_fr.php
+++ lang/paquet-squelettes_par_rubrique_fr.php
@@ -1,19 +1,14 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-// Fichier produit par PlugOnet
-// Module: paquet-squelettes_par_rubrique
-// Langue: fr
-// Date: 30-07-2011 15:01:38
-// Items: 2
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_core_/plugins/squelettes_par_rubrique/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// S
+	// S
 	'squelettes_par_rubrique_description' => 'Support des squelettes suffixés par numéro de rubrique et/ou par code de langue :
 (-23.html, =23.html, et .en.html)',
-	'squelettes_par_rubrique_slogan' => 'Support des squelettes suffixés dans SPIP',
+	'squelettes_par_rubrique_slogan' => 'Support des squelettes suffixés dans SPIP'
 );
+
 ?>
