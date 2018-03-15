5bc896ebe78e6e6d3de535507def446341e2f8e9
gpl <gpl@long-player.net>
2018-03-15 04:42:14 +0000

    [Salvatore] [source:_core_/plugins/squelettes_par_rubrique/lang/ paquet-squelettes_par_rubrique] Export depuis http://trad.spip.net de la langue eo

---
+++ lang/paquet-squelettes_par_rubrique_eo.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-squelettes_par_rubrique?lang_cible=eo
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// S
+	'squelettes_par_rubrique_description' => 'Subteno por la skeletoj sufiksitaj per rubrikonumero kaj lingvokodo (ekz., -23.html, =23.html, kaj .eo.html)',
+	'squelettes_par_rubrique_slogan' => 'Subteno por la sufiksitaj skeletoj en SPIP'
+);
